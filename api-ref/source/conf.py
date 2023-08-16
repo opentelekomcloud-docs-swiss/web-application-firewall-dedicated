899e20ca07cae1983998e8443c96633144835ea7
vladimirhasko <vladimirhasko@gmail.com>
2023-08-16 20:16:25 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata-swiss/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -23,11 +23,21 @@ extensions = [
 
 otcdocs_auto_name = False
 otcdocs_auto_version = False
-otcdocs_search_environment = 'hc_swiss'
 
 project = 'Dedicated Web Application Firewall'
 otcdocs_repo_name = 'opentelekomcloud-docs-swiss/web-application-firewall-dedicated'
 # Those variables are required for edit/bug links
+
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/web-application-firewall-dedicated/api-ref/'
+otcdocs_doc_title = 'API Reference'
+otcdocs_doc_type = 'api-ref'
+otcdocs_service_category = 'security-services'
+otcdocs_service_title = 'Dedicated Web Application Firewall'
+otcdocs_service_type = 'wafd'
+otcdocs_search_environment = 'hc_swiss'
+otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
